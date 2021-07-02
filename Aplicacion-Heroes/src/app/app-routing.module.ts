5520b99d48d069c20a9f6c4c7144f423415c5556
Daniel151505 <daniel150520011@gmail.com>
2021-07-01 23:41:56 -0500

    Agregando Ruta

--- Aplicacion-Heroes/src/app/app-routing.module.ts
+++ Aplicacion-Heroes/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path: 'about' , component: AboutComponent},
   {path: 'heroes' , component: HeroesComponent},
   {path: 'heroe/:id' , component: HeroeComponent},
-  {path: 'heroe/:termino' , component: BuscadorComponent},
+  {path: 'buscar/:termino' , component: BuscadorComponent},
   {path: '**', pathMatch: 'full' ,redirectTo: 'home' }
 ];
 
